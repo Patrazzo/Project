8bede67e6e2129d818c42c7bb22d9cdb2cfac040
Patrazzo <petarpetrov092@gmail.com>
2023-04-16 12:41:36 +0300

    Bug Fixes

--- AutoRentals.com/Contact/Contact.php
+++ AutoRentals.com/Contact/Contact.php
@@ -1,35 +1,31 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
+// the message
+$msg = "First line of text\nSecond line of text";
 
-if(isset($_POST["send"])){
-    $mail = new PHPMailer(true);
-    
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'autorentals2@gmail.com';
-    $mail->Password = 'tdgnttpbcnkhbzhd';
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port = 465;
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
 
-    $mail->setFrom('autorentals2@gmail.com');
+// send email
+mail("yolalo8303@gam1fy.com","My subject",$msg);
 
-    $mail->addAddress($_POST["email"]);
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     $fname = $_POST["fname"];
+//     $lname = $_POST["lname"];
+//     $email = $_POST["email"];
+//     $message = $_POST["message"];
 
-    $mail->isHTML(true);
-    $mail->Subject = 'Contact Form Submission';
-    $mail->Body = 'First Name: ' . $_POST["fname"] . '<br>Last Name: ' . $_POST["lname"] . '<br>Message: ' . $_POST["message"];
+//     $to = "dereg70898@ippals.com"; // Replace with your email address
+//     $subject = "Contact Form Submission";
+//     $body = "First Name: " . $fname . "\n" .
+//         "Last Name: " . $lname . "\n" .
+//         "Email: " . $email . "\n" .
+//         "Message: " . $message;
 
-    if($mail->send()){
-        echo '<script>alert("Message sent successfully!");</script>';
-    }
-    else{
-        echo '<script>alert("Message could not be sent. Please try again later.");</script>';
-    }
-}
+//     if (mail($to, $subject, $body)) {
+//         echo "<script>alert('Thank you for your message! We will get back to you shortly.');</script>";
+//     } else {
+//         echo "<script>alert('Sorry, there was an error sending your message. Please try again later.');</script>";
+//     }
+// }
 ?>
