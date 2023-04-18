d8e7c74a1bee6e4a85b7201a79ce9152ecd1fbca
NikolaRastashki <noki_tr@abv.bg>
2023-04-18 20:17:36 +0300

    About top up

--- AutoRentals.com/About/About.js
+++ AutoRentals.com/About/About.js
@@ -14,3 +14,15 @@ window.addEventListener('scroll', function() {
       }
     }
   });
+
+  function scrollToTop() {
+    const scrollDuration = 2000; // задава времето в милисекунди за преминаване до началото на страницата
+    const scrollStep = -window.scrollY / (scrollDuration / 100); // задава броя на превъртанията на страницата във всяка стъпка
+    const scrollInterval = setInterval(() => {
+        if (window.scrollY !== 0) {
+            window.scrollBy(0, scrollStep);
+        } else {
+            clearInterval(scrollInterval);
+        }
+    }, 10);
+}
