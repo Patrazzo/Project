d10ee0e9c7320d34f4ee0ef248c2aaf035178fa1
Ivak <ivoslavpetkov@gmail.com>
2023-05-29 12:29:29 +0300

    Updated php for register

--- AutoRental.com/Register/Functionality/RegisterEN.php
+++ AutoRental.com/Register/Functionality/RegisterEN.php
@@ -1,12 +1,8 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-
 include '../../Config/config.php';
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
 
     $firstName = mysqli_real_escape_string($conn, $_POST['firstName']);
     $lastName = mysqli_real_escape_string($conn, $_POST['lastName']);
@@ -14,20 +10,21 @@ if(isset($_POST['submit'])){
     $hashedPass = password_hash($pass, PASSWORD_DEFAULT);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     $phoneNumber = mysqli_real_escape_string($conn, $_POST['phoneNumber']);
- 
-    $stmt = mysqli_prepare($conn, "SELECT * FROM `users` WHERE email = ?") or die('query preparation failed');
+
+    $stmt = mysqli_prepare($conn, "SELECT * FROM `users` WHERE email = ?");
     mysqli_stmt_bind_param($stmt, "s", $email);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
- 
-    if(mysqli_num_rows($result) > 0){
-       $message[] = 'User already exists!';
-    }else{
-        $stmt = mysqli_prepare($conn, "INSERT INTO `users`(firstName, lastName, pass, email, phoneNumber) VALUES(?, ?, ?, ?, ?)") or die('query preparation failed');
+
+    if (mysqli_num_rows($result) > 0) {
+        $message = 'User already exists!';
+    } else {
+        $stmt = mysqli_prepare($conn, "INSERT INTO `users`(firstName, lastName, pass, email, phoneNumber) VALUES(?, ?, ?, ?, ?)");
         mysqli_stmt_bind_param($stmt, "sssss", $firstName, $lastName, $hashedPass, $email, $phoneNumber);
         mysqli_stmt_execute($stmt);
-        $message[] = 'Registered successfully!';
+        $message = 'Registered successfully!';
         header('location: ../../Home/EN/Home.html');
+        exit;
     }
 }
 ?>
