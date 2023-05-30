2cf431b841189203e4d297a9c80586f6ae4f1eae
Ivak <ivoslavpetkov@gmail.com>
2023-05-30 11:32:43 +0300

    Reworked php for login
    ONLY EN WORKING

--- AutoRental.com/Register/Functionality/RegisterEN.php
+++ AutoRental.com/Register/Functionality/RegisterEN.php
@@ -22,7 +22,6 @@ if (isset($_POST['submit'])) {
         $stmt = mysqli_prepare($conn, "INSERT INTO `users`(firstName, lastName, pass, email, phoneNumber) VALUES(?, ?, ?, ?, ?)");
         mysqli_stmt_bind_param($stmt, "sssss", $firstName, $lastName, $hashedPass, $email, $phoneNumber);
         mysqli_stmt_execute($stmt);
-        $message = 'Registered successfully!';
         header('location: ../../Home/EN/Home.html');
         exit;
     }
