b216faf87f9f4920a775413501e2bdacd7e9ce23
aaaaaaaaaaaaav <130397432+aaaaaaaaaaaaav@users.noreply.github.com>
2023-05-31 08:25:49 +0300

    PESHOO

--- AutoRental.com/GeneralStyling&Media/Header/Header.js
+++ AutoRental.com/GeneralStyling&Media/Header/Header.js
@@ -1,4 +1,10 @@
 document.querySelector(".menu-toggle").addEventListener("click", function () {
-    this.classList.toggle("active");
-    document.querySelector(".menu").classList.toggle("active");
-      });
+  this.classList.toggle("active");
+  document.querySelector(".menu").classList.toggle("active");
+
+  // Промяна на вида на бутона
+  var spans = this.querySelectorAll("span");
+  spans.forEach(function (span) {
+    span.classList.toggle("change");
+  });
+});
