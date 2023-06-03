328903028c12131e8f93d54040e8140c450d179c
Ivak <ivoslavpetkov@gmail.com>
2023-06-03 18:01:05 +0300

    Fixed a minor bug

--- AutoRental.com/Home/EN/HomeA.php
+++ AutoRental.com/Home/EN/HomeA.php
@@ -1,16 +1,16 @@
 <?php
 session_start();
-
-$users_id = $_SESSION['users_id'];
 $firstName = $_SESSION['firstName'];
+$utype = $_SESSION['utype'];
 
-
-if(!isset($users_id)){
-   header('location:../../Login/EN/Login.html');
+if ($_SESSION['utype'] !== 'admin') {
+    header('location:../../Login/EN/Login.html');
+    exit();
 }
-else{
+else {
     echo "Hello, admin $firstName";
 }
+
 ?>
 
 <!DOCTYPE html>
