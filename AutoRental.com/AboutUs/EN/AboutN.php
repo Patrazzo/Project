68b642c0edbd8f9c632da6cf29024dd9aa264faf
Patrazzo <petarpetrov092@gmail.com>
2023-06-07 11:51:50 +0300

    AboutUS simplified HTML&CSS

--- AutoRental.com/AboutUs/EN/AboutN.php
+++ AutoRental.com/AboutUs/EN/AboutN.php
@@ -5,7 +5,7 @@ echo "Hello, stranger";
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="bg">
 
 <head>
     <meta charset="UTF-8">
@@ -19,42 +19,42 @@ echo "Hello, stranger";
     <link rel="stylesheet" href="../../GeneralStyling&Media/Header/Header.css">
     <link rel="stylesheet" href="../Styling/About.css">
     <link rel="stylesheet" href="../../GeneralStyling&Media/Footer/Footer.css">
-
-
 </head>
 
 <body>
+    <!-- OБЩ КОНТЕЙНЕР -->
     <div class="all">
+        <!-- КОНТЕЙНЕР ЗА ХЕДЪР -->
         <div class="header">
-
+            <!-- КОНТЕЙНЕР ЗА ЛОГО -->
             <div class="logo">
                 <a href="../../Home/EN/HomeN.php"><img id="original-logo"
                         src="../../GeneralStyling&Media/Photos/Logo.png"></a>
                 <a href="../../Home/EN/HomeN.php"><img id="hovered-logo"
                         src="../../GeneralStyling&Media/Photos/HoverLogo.png"></a>
             </div>
-
+            <!-- КОНТЕЙНЕР ЗА ЛИНКОВЕ -->
             <div class="links">
                 <a href="../../Catalog/EN/CatalogN.php">КАТАЛОГ</a>
                 <a id="clicked" href="../../AboutUs/EN/AboutN.php">ЗА НАС</a>
                 <a href="../../Contact/EN/ContactN.php">КОНТАКТ</a>
             </div>
-
+            <!-- КОНТЕЙНЕР ЗА БУРГЕР МЕНЮ -->
             <div class="menu-toggle">
                 <span></span>
                 <span></span>
                 <span></span>
             </div>
-
+            <!-- КОНТЕЙНЕР ЗА ДРОПДАУН -->
             <div class="menu">
                 <a href="../../Catalog/EN/CatalogN.php">КАТАЛОГ</a>
                 <a id="clicked" href="../../AboutUs/EN/AboutN.php">ЗА НАС</a>
                 <a href="../../Contact/EN/ContactN.php">КОНТАКТ</a>
                 <a href="../../Login/EN/Login.html">ВЛИЗАНЕ</a>
             </div>
-
+            <!-- ИМПЛЕМЕНТАЦИЯ НA JS ЗА РАБОТА НА ДРОПДАУНА -->
             <script src="../../GeneralStyling&Media/Header/Header.js"></script>
-
+            <!-- КОНТЕЙНЕР ЗА ЛОГИН -->
             <div class="login">
                 <a href="../../Login/EN/Login.html"><img id="original-login"
                         src="../../GeneralStyling&Media/Photos/Login.png"></a>
@@ -63,12 +63,17 @@ echo "Hello, stranger";
             </div>
         </div>
 
+        <!-- КОНТЕЙНЕР ЗА МЕЙН -->
         <div class="main">
+            <!-- КОНТЕЙНЕР ЗА СЪДЪРЖАНИЕТО -->
             <div class="TextContainer">
+                <!-- КОНТЕЙНЕР ЗА ЗАГЛАВИЕ -->
                 <div class="Heading">
                     <h2>КОИ СМЕ НИЕ</h2>
                 </div>
+                <!-- КОНТЕЙНЕР ЗА ТЕМА -->
                 <div class="Row">
+                    <!-- КОНТЕЙНЕР ЗА ТЕКСТ -->
                     <div class="text">
                         <p>
                             Добре дошли в нашата компания за луксозни коли под наем, където предлагаме разнообразна гама
@@ -87,20 +92,23 @@ echo "Hello, stranger";
                             с нас до края на вашия наем.
                         </p>
                     </div>
+                    <!-- КОНТЕЙНЕР ЗА СНИМКА -->
                     <div class="image">
                         <img src="../../GeneralStyling&Media/Photos/Dealership.jpg">
                     </div>
                 </div>
 
-
-
+                <!-- КОНТЕЙНЕР ЗА ЗАГЛАВИЕ -->
                 <div class="Heading">
                     <h2>НАШАТА МИСИЯ</h2>
                 </div>
+                <!-- КОНТЕЙНЕР ЗА ТЕМА -->
                 <div class="Row">
+                    <!-- КОНТЕЙНЕР ЗА СНИМКА -->
                     <div class="image">
                         <img src="../../GeneralStyling&Media/Photos/keys.png">
                     </div>
+                    <!-- КОНТЕЙНЕР ЗА ТЕКСТ -->
                     <div class="text">
                         <p>
                             Нашата мисия е проста - да предоставим на нашите клиенти възможно най-доброто изживяване при
@@ -121,11 +129,13 @@ echo "Hello, stranger";
                     </div>
                 </div>
 
-
+                <!-- КОНТЕЙНЕР ЗА ЗАГЛАВИЕ -->
                 <div class="Heading">
                     <h2>КАКВО ПРЕДЛАГАМЕ</h2>
                 </div>
+                <!-- КОНТЕЙНЕР ЗА ТЕМА -->
                 <div class="Row">
+                    <!-- КОНТЕЙНЕР ЗА ТЕКСТ -->
                     <div class="text">
                         <p>
                             В нашата компания за коли под наем разбираме, че наемането на кола е повече от транзакция -
@@ -141,25 +151,23 @@ echo "Hello, stranger";
                             повечето комплекс ремонти.
                         </p>
                     </div>
+                    <!-- КОНТЕЙНЕР ЗА СНИМКА -->
                     <div class="image">
                         <img src="../../GeneralStyling&Media/Photos/Dealership.jpg">
                     </div>
                 </div>
             </div>
         </div>
+        <!-- КОНТЕЙНЕР ЗА SCROLL-TO-TOP СНИМКА -->
         <div class="scroller">
             <a onclick="scrollToTop(); return false;"><img src="../../GeneralStyling&Media/Photos/Logo.png"
                     width="300"></a>
         </div>
     </div>
 
+    <!-- КОНТЕЙНЕР ЗА FOOTER -->
     <div class="footer">
         <h5>| Copyright © 2023 AutoRental | Всички права запазени |</h5>
     </div>
-
-
-
-    </div>
 </body>
-
 </html>
