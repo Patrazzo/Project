ae36baeedeff3867af25b659d345a66593090179
Ivak <ivoslavpetkov@gmail.com>
2023-06-07 08:34:22 +0300

    Fixed admin panels and added last table

--- AutoRental.com/Database/AutoRental.sql
+++ AutoRental.com/Database/AutoRental.sql
@@ -35,3 +35,12 @@ CREATE TABLE orders (
     FOREIGN KEY (carId) REFERENCES catalog(id),
     FOREIGN KEY (userId) REFERENCES users(users_id)
 );
+
+CREATE TABLE messages (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  topic VARCHAR(100),
+  email VARCHAR(100),
+  message TEXT
+);
