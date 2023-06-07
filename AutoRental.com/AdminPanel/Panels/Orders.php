098f68cb2ca839e3e80b31dbd2cc8b93a82c12cb
Ivak <ivoslavpetkov@gmail.com>
2023-06-07 11:34:18 +0300

    Added logic to the catalog(waiting for the css and html)

--- AutoRental.com/AdminPanel/Panels/Orders.php
+++ AutoRental.com/AdminPanel/Panels/Orders.php
@@ -1,8 +1,59 @@
+<?php
+// Start the session and include the necessary files
+session_start();
+include '../../Config/config.php';
+$utype = $_SESSION['utype'];
+
+// Check if the user is logged in, else redirect to the login page
+if ($_SESSION['utype'] !== 'admin') {
+    header('location: ../../Login/EN/Login.html');
+    exit();
+}
+
+// Check if the form is submitted to add a new product
+if (isset($_POST['add_product'])) {
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $description = mysqli_real_escape_string($conn, $_POST['description']);
+    $image = mysqli_real_escape_string($conn, $_POST['image']);
+    $price = mysqli_real_escape_string($conn, $_POST['price']);
+
+    // Insert the new product into the catalog table
+    $insertQuery = "INSERT INTO catalog (name, description, image, price, created_at) VALUES ('$name', '$description', '$image', '$price', NOW())";
+    mysqli_query($conn, $insertQuery);
+}
+
+// Check if the form is submitted to update a product
+if (isset($_POST['update_product'])) {
+    $id = mysqli_real_escape_string($conn, $_POST['id']);
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $description = mysqli_real_escape_string($conn, $_POST['description']);
+    $image = mysqli_real_escape_string($conn, $_POST['image']);
+    $price = mysqli_real_escape_string($conn, $_POST['price']);
+
+    // Update the product in the catalog table
+    $updateQuery = "UPDATE catalog SET name='$name', description='$description', image='$image', price='$price' WHERE id=$id";
+    mysqli_query($conn, $updateQuery);
+}
+
+// Check if the form is submitted to delete a product
+if (isset($_POST['delete_product'])) {
+    $id = mysqli_real_escape_string($conn, $_POST['id']);
+
+    // Delete the product from the catalog table
+    $deleteQuery = "DELETE FROM catalog WHERE id=$id";
+    mysqli_query($conn, $deleteQuery);
+}
+
+// Retrieve the products from the catalog table
+$query = "SELECT * FROM catalog";
+$result = mysqli_query($conn, $query);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../../GeneralStyling&Media/Photos/Logo.png">
@@ -15,123 +66,69 @@
 </head>
 
 <body>
-    <div class="all">
-        <div class="header">
-            <div class="logo">
-                <a href="../Panels/Dashboard.php"><img id="original-logo"
-                        src="../../GeneralStyling&Media/Photos/Logo.png"></a>
-                <a href="../Panels/Dashboard.php"><img id="hovered-logo"
-                        src="../../GeneralStyling&Media/Photos/HoverLogo.png"></a>
-            </div>
-
-            <div class="links">
-                <a href="../Panels/Products.php">Products</a>
-                <a id="clicked" href="../Panels/Orders.php">Orders</a>
-                <a href="../Panels/Account.php">Users</a>
-                <a href="../Panels/Messages.php">Messages</a>
-            </div>
-
-            <div class="menu-toggle">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-
-            <div class="menu">
-                <a href="../Panels/Dashboard.html">Dashboard</a>
-                <a href="../Panels/Products.html">Products</a>
-                <a id="clicked" href="../Panels/Orders.html">Orders</a>
-                <a href="../Panels/Account.html">Users</a>
-                <a href="../Panels/Messages.html">Messages</a>
-            </div>
-
-            <script src="../../GeneralStyling&Media/Header/Header.js"></script>
-
-            <div class="login">
-                <?php if (isset($_GET['logout']) && $_GET['logout'] == 'true'): ?>
-                    <a href="../../Home/EN/HomeN.php?logout=true"><img id="original-login"
-                            src="../../GeneralStyling&Media/Photos/Login.png"></a>
-                    <a href="../../Home/EN/HomeN.php?logout=true"><img id="hovered-login"
-                            src="../../GeneralStyling&Media/Photos/HoverLogin.png"></a>
-                <?php else: ?>
-                    <a href="../../Home/EN/HomeN.php?logout=true"><img id="original-login"
-                            src="../../GeneralStyling&Media/Photos/Login.png"></a>
-                    <a href="../../Home/EN/HomeN.php?logout=true"><img id="hovered-login"
-                            src="../../GeneralStyling&Media/Photos/HoverLogin.png"></a>
-                <?php endif; ?>
-            </div>
+    <!-- HTML content goes here -->
+    <div class="main">
+        <div class="container">
+            <h1>ORDERS</h1>
+                <!-- Display the existing products -->
+<?php while ($row = mysqli_fetch_assoc($result)) : ?>
+    <div class="order">
+        <div class="row">
+            <p>Name</p>
+            <h6><?php echo $row['name']; ?></h6>
+        </div>
+        <div class="row">
+            <p>Description</p>
+            <h6><?php echo $row['description']; ?></h6>
+        </div>
+        <div class="row">
+            <p>Image</p>
+            <img src="../../GeneralStyling&Media/Photos/cars/<?php echo $row['image']; ?>" alt="Product Image">
+        </div>
+        <div class="row">
+            <p>Price</p>
+            <h6><?php echo $row['price']; ?></h6>
         </div>
 
+        <!-- Edit and delete options for each product -->
+        <div class="row">
+            <div class="options">
+                <!-- Edit Form -->
+                <form method="POST" action="">
+                    <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+                    <input type="text" name="name" value="<?php echo $row['name']; ?>">
+                    <input type="text" name="description" value="<?php echo $row['description']; ?>">
+                    <input type="text" name="image" value="<?php echo $row['image']; ?>">
+                    <input type="text" name="price" value="<?php echo $row['price']; ?>">
+                    <button type="submit" name="update_product">Update</button>
+                </form>
 
-        <div class="main">
-            <div class="container">
-                <h1>ORDERS</h1>
-                <div class="order">
-                    <div class="row">
-                        <p>UserID</p>
-                        <h6>1</h6>
-                    </div>
-                    <div class="row">
-                        <p>placed on</p>
-                        <h6>1</h6>
-                    </div>
-                    <div class="row">
-                        <p>Name</p>
-                        <h6>Ivoslav</h6>
-                    </div>
-                    <div class="row">
-                        <p>Number</p>
-                        <h6>0882029658</h6>
-                    </div>
-                    <div class="row">
-                        <p>email</p>
-                        <h6>19213@uktc-bg.com</h6>
-                    </div>
-                    <div class="row">
-                        <p>Address</p>
-                        <h6>
-                            Димитър Стойчев
-                        </h6>
-                    </div>
-                    <div class="row">
-                        <p>Total products</p>
-                        <h6>
-                            Димитър Стойчев
-                        </h6>
-                    </div>
-                    <div class="row">
-                        <p>Total price</p>
-                        <h6>
-                            Димитър Стойчев
-                        </h6>
-                    </div>
-                    <div class="row">
-                        <p>payment method</p>
-                        <h6>
-                            Димитър Стойчев
-                        </h6>
-                    </div>
-                    <div id="center" class="row">
-                        <select>
-                            <option value="option1">Option 1</option>
-                            <option value="option2">Option 2</option>
-                            <option value="option3">Option 3</option>
-                        </select>
-                    </div>
-                    <div id="center" class="row">
-                        <div class="options">
-                            <button>Delete</button>
-                            <button>Update</button>
-                        </div>
-                    </div>
-
-                </div>
+                <!-- Delete Form -->
+                <form method="POST" action="">
+                    <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+                    <button type="submit" name="delete_product">Delete</button>
+                </form>
             </div>
-        </div>
-        <div class="footer">
-            <h5>AutoRental | AdminPanel</h5>
         </div>
     </div>
+<?php endwhile; ?>
+
+
+            <!-- Add new product form -->
+            <div class="order">
+                <h3>Add New Product</h3>
+                <form method="POST" action="">
+                    <input type="text" name="name" placeholder="Product Name" required>
+                    <input type="text" name="description" placeholder="Product Description" required>
+                    <input type="text" name="image" placeholder="Product Image" required>
+                    <input type="text" name="price" placeholder="Product Price" required>
+                    <button type="submit" name="add_product">Add Product</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer content goes here -->
 </body>
 
 </html>
