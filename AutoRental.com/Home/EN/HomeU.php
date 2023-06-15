e91e7aba13c5b52c73f84863b85a276313d7d9d2
Patrazzo <petarpetrov092@gmail.com>
2023-06-15 17:09:22 +0300

    Removed back up
    still have it on my pc chill

--- AutoRental.com/Home/EN/HomeU.php
+++ AutoRental.com/Home/EN/HomeU.php
@@ -23,6 +23,7 @@ if ($_SESSION['utype'] !== 'user') {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Начало | AutoRental</title>
+    <script src="../Functionality/Home.js"></script>
     <link rel="stylesheet" href="../../GeneralStyling&Media/General/General.css">
     <link rel="stylesheet" href="../../GeneralStyling&Media/Header/Header.css">
     <link rel="stylesheet" href="../Styling/Home.css">
